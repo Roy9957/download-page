c654651369b6f48506128140fafd1be386c12ee5
Roy9957 <ranobirroy221@gmail.com>
2025-05-13 18:06:34 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Download Our Awesome App</h1>
+      <img
+        src="https://assets.onecompiler.app/42swzbgaa/43hm2rkk7/1000071470.png"
+        alt="App Preview"
+        className="app-image"
+      />
+      <a
+        href="/download/MOBE.apk"
+        className="download-button"
+        download
+      >
+        <i className="fas fa-download"></i> Download Now
+      </a>
+    </div>
+  );
+}
+
+export default App;
