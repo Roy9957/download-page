a01823fc2b3278997348ff8f9a3fe9a1ac9c75e8
MOBE WORKS <ranobirroy221@gmail.com>
2025-05-13 18:42:17 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1 className="title">Download Our Awesome App</h1>
+      <h1 className="title">RONOBIR ROY(MOBE WORKS)</h1>
       <img
         src="https://assets.onecompiler.app/42swzbgaa/43hm2rkk7/1000071470.png"
         alt="App Preview"
@@ -14,7 +14,7 @@ function App() {
         className="download-button"
         download
       >
-        <i className="fas fa-download"></i> Download Now
+        <i className="fas fa-download"></i> Download
       </a>
     </div>
   );
